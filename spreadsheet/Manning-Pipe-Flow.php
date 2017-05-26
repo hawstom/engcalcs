a385018a3c34dde7a7c9e33d85ef4eecf06fab63
Tom Haws <tom.haws@gmail.com>
2017-05-26 13:23:12 -0400

    Added spreadsheet redirectors.

---
+++ spreadsheet/Manning-Pipe-Flow.php
@@ -0,0 +1,7 @@
+<?php
+//header('Location: https://drive.google.com/open?id=1bqi_n1mcrF1PArUK9Gp81CDlgV0GpmP7GK_okjgI3MQ', true, 301);
+//exit();
+header("Refresh: 1; url = https://drive.google.com/open?id=1bqi_n1mcrF1PArUK9Gp81CDlgV0GpmP7GK_okjgI3MQ");
+?>
+
+<a href="https://drive.google.com/open?id=1bqi_n1mcrF1PArUK9Gp81CDlgV0GpmP7GK_okjgI3MQ">Click here if not redirected in 3 seconds</a>
