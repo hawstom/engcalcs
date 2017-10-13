77ea2a3aae0d39bb7251272b245817cf78482a29
Tom Haws <tom.haws@gmail.com>
2017-10-13 14:05:57 -0400

    Work by Ace on calculators

--- SpreadsheetLibrary.php
+++ SpreadsheetLibrary.php
@@ -11,12 +11,24 @@ echoHeader("EngCalcs", $html_title, $html_head);
 <h2>All Spreadsheets</h2>
 
 <h4>Manning Pipe Flow Calculator:</h4>&ensp;
+<!--
 <a href = "spreadsheet/Manning-Pipe-Flow_1.0.ods">1.0 OpenDocument</a></br>&ensp;
+-->
 <a href = "spreadsheet/Manning-Pipe-Flow_1.0.php">1.0 Google Sheets</a></br>&ensp;
+<!--
 <a href = "spreadsheet/Manning-Pipe-Flow_1.1.ods">1.1 OpenDocument</a></br>&ensp;
+-->
 <a href = "spreadsheet/Manning-Pipe-Flow_1.1.php">1.1 Google Sheets</a></br>&ensp;<b>
+<!--
 <a href = "spreadsheet/Manning-Pipe-Flow_2.0.ods">2.0 OpenDocument</a></b></br>&ensp;<b>
-<a href = "spreadsheet/Manning-Pipe-Flow_2.0.php">2.0 Google Sheets</a></b></p>
+-->
+<a href = "spreadsheet/Manning-Pipe-Flow_2.0.xlsx">2.0 Microsoft Excel</a></b></br>&ensp;<b>
+<a href = "spreadsheet/Manning-Pipe-Flow_2.0.php">2.0 Google Sheets</a></b>
+</p>
+<!--
+--CAUTION: You may receive incorrect results using the OpenDocument spreadsheet in Microsoft Excel!--
+-->
+</p>
 
 <?php
 echoFooter();
