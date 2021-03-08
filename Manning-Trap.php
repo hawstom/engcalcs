248139ad6219a60bbb5bd9c0ea33b2c00e529af5
Tom Haws <tom.haws@gmail.com>
2021-03-07 19:23:08 -0700

    Added Blodgett and Bathurst to Trap. Also reworked Strickler approach.

--- Manning-Trap.php
+++ Manning-Trap.php
@@ -22,7 +22,8 @@ echoCalculatorForm(
 		Array('name' => 's0', 'type' => 'number', 'default' => '0.001', 'units' => Array('grade', 'gradePercent'), 'label' => $ec_lang['mtc_channel_slope']),
 		Array('name' => 'y', 'type' => 'number', 'default' => '1', 'units' => Array('m', 'mm', 'ft', 'in'), 'label' => $ec_lang['mtc_flow_depth']),
 		Array('name' => 'beta', 'type' => 'number', 'default' => '0', 'units' => NULL, 'label' => $ec_lang['mtc_bend_angle']),
-		Array('name' => 'sgrock', 'type' => 'number', 'default' => '2.65', 'units' => NULL, 'label' => $ec_lang['mtc_sgrock'])
+		Array('name' => 'sgrock', 'type' => 'number', 'default' => '2.65', 'units' => NULL, 'label' => $ec_lang['mtc_sgrock']),
+		Array('name' => 'd50in', 'type' => 'number', 'default' => '1', 'units' => Array('m', 'mm', 'ft', 'in'), 'label' => $ec_lang['mtc_d50in']),
 	),
 	//Results
 	Array(
@@ -35,7 +36,10 @@ echoCalculatorForm(
 		Array('name' => 't', 'units' => Array('m', 'mm', 'ft', 'in'), 'label' => $ec_lang['mpf_top_width']),
 		Array('name' => 'froude', 'units' => NULL, 'label' => $ec_lang['mpf_froude_number']),
 		Array('name' => 'tau', 'units' => Array('npm2', 'psf'), 'label' => $ec_lang['mpf_shear_stress']),
-		Array('name' => 'd50_strickler', 'units' => Array('m', 'mm', 'ft', 'in'), 'label' => $ec_lang['mtc_d50_strickler']),
+		Array('name' => 'n_strickler', 'units' => NULL, 'label' => $ec_lang['mtc_n_strickler']),
+		Array('name' => 'n_blodgett', 'units' => NULL, 'label' => $ec_lang['mtc_n_blodgett']),
+		Array('name' => 'n_bathurst', 'units' => NULL, 'label' => $ec_lang['mtc_n_bathurst']),
+		Array('name' => 'blodgett_v_bathurst', 'units' => NULL, 'label' => $ec_lang['mtc_blodgett_v_bathurst']),
 		Array('name' => 'd50_bottom', 'units' => Array('m', 'mm', 'ft', 'in'), 'label' => $ec_lang['mtc_d50_bottom']),
 		Array('name' => 'd50_z1', 'units' => Array('m', 'mm', 'ft', 'in'), 'label' => $ec_lang['mtc_d50_z1']),
 		Array('name' => 'd50_z2', 'units' => Array('m', 'mm', 'ft', 'in'), 'label' => $ec_lang['mtc_d50_z2']),
@@ -53,8 +57,10 @@ EngCalcs.pageCalculator = function(objForm) {
 	'use strict';
 	var hasUnits, precision;
 	this.var = {};
-	this.var.c = 1.0;
 	this.var.g = 9.806;
+	this.var.c = 1.0;
+	this.var.alpha_blodgett = 0.319;
+	this.var.alpha_bathurst = 1.0;
 	// Read and convert form inputs to this.var.___ as SI units
 	this.readFormInput(objForm, 'b', hasUnits = true);
 	this.readFormInput(objForm, 'y', hasUnits = true);
@@ -64,22 +70,28 @@ EngCalcs.pageCalculator = function(objForm) {
 	this.readFormInput(objForm, 'n', hasUnits = false);
 	this.readFormInput(objForm, 'beta', hasUnits = false);
 	this.readFormInput(objForm, 'sgrock', hasUnits = false);
+	this.readFormInput(objForm, 'd50in', hasUnits = true);
 	this.var.a = this.var.y * (this.var.b + (+this.var.z1 + +this.var.z2) * this.var.y / 2);
 	this.var.pw = this.var.b + this.var.y * (Math.sqrt(1 + Math.pow(this.var.z1, 2)) + Math.sqrt(1 + Math.pow(this.var.z2, 2)));
 	this.var.rh = this.var.a / this.var.pw;
 	this.var.t = this.var.b + this.var.y * (+this.var.z1 + +this.var.z2);
+	this.var.da = this.var.a / this.var.t;
+	this.var.da_over_d50 = this.var.da / this.var.d50in;
 	this.var.v = this.var.c/this.var.n*Math.pow(this.var.rh,2/3)*Math.pow(this.var.s0,0.5);
 	this.var.hv=Math.pow(this.var.v, 2) / (2 * this.var.g)
 	this.var.q = this.var.v * this.var.a;
 	this.var.froude = this.var.v * Math.sqrt(this.var.t/(this.var.g * this.var.a * Math.cos(Math.atan(this.var.s0))));
 	this.var.tau = this.var.rh * this.var.s0;
 	this.var.c_isbash = (this.var.beta <= 30) ? 1.2 : 0.86;
-	this.var.d50_strickler = Math.pow(this.var.n * 21.1, 6); // n = 1/21.1 D ^ (1/6)
+	this.var.n_strickler = Math.pow(this.var.d50in, 1 / 6) / 21.1;
+	this.var.n_blodgett = this.var.alpha_blodgett * Math.pow(this.var.da, 1/6) / (2.25 + 5.23 * Math.log10(this.var.da/this.var.d50in));
+	this.var.n_bathurst = this.bathurst_n(this.var.alpha_bathurst, this.var.g, this.var.t, this.var.da, this.var.d50in, this.var.froude);
+	this.var.blodgett_v_bathurst = (this.var.da_over_d50 < 0.3) ? '----' : (this.var.da_over_d50 < 1.5) ? 'Bathurst' : (this.var.da_over_d50 <= 185) ? 'Blodgett' : '++++';
 	this.var.d50_mra = 0.031 * Math.pow(this.var.v, 2.5) / (Math.pow(this.var.sgrock - 1, 0.25) * Math.pow(this.var.y, 0.25) * ((this.var.beta <= 30) ? 1 : 1.5));
 	this.var.d50_searcy = 0.022 * Math.pow(this.var.v, 2);
-	this.var.d50_bottom = mc_riprap_size(this.var.y, this.var.a, this.var.v, this.var.g, 1000, this.var.s0, this.var.c_isbash, this.var.sgrock);
-	this.var.d50_z1 = mc_riprap_size(this.var.y, this.var.a, this.var.v, this.var.g, this.var.z1, this.var.s0, this.var.c_isbash, this.var.sgrock);
-	this.var.d50_z2 = mc_riprap_size(this.var.y, this.var.a, this.var.v, this.var.g, this.var.z2, this.var.s0, this.var.c_isbash, this.var.sgrock);
+	this.var.d50_bottom = this.mc_riprap_size(this.var.y, this.var.a, this.var.v, this.var.g, 1000, this.var.s0, this.var.c_isbash, this.var.sgrock);
+	this.var.d50_z1 = this.mc_riprap_size(this.var.y, this.var.a, this.var.v, this.var.g, this.var.z1, this.var.s0, this.var.c_isbash, this.var.sgrock);
+	this.var.d50_z2 = this.mc_riprap_size(this.var.y, this.var.a, this.var.v, this.var.g, this.var.z2, this.var.s0, this.var.c_isbash, this.var.sgrock);
 	this.writeFormResult(objForm, 'a', precision = 4, hasUnits = true);
 	this.writeFormResult(objForm, 'pw', precision = 4, hasUnits = true);
 	this.writeFormResult(objForm, 'rh', precision = 4, hasUnits = true);
@@ -89,7 +101,10 @@ EngCalcs.pageCalculator = function(objForm) {
 	this.writeFormResult(objForm, 't', precision = 4, hasUnits = true);
 	this.writeFormResult(objForm, 'froude', precision = 2, hasUnits = false);
 	this.writeFormResult(objForm, 'tau', precision = 4, hasUnits = true);
-	this.writeFormResult(objForm, 'd50_strickler', precision = 4, hasUnits = true);
+	this.writeFormResult(objForm, 'n_strickler', precision = 3, hasUnits = false);
+	this.writeFormResult(objForm, 'n_blodgett', precision = 3, hasUnits = false);
+	this.writeFormResult(objForm, 'n_bathurst', precision = 3, hasUnits = false);
+	document.getElementById('blodgett_v_bathurst').innerHTML = (this.var.blodgett_v_bathurst);
 	this.writeFormResult(objForm, 'd50_bottom', precision = 4, hasUnits = true);
 	this.writeFormResult(objForm, 'd50_z1', precision = 4, hasUnits = true);
 	this.writeFormResult(objForm, 'd50_z2', precision = 4, hasUnits = true);
@@ -132,24 +147,6 @@ EngCalcs.pageCalculator = function(objForm) {
 };
 EngCalcs.pageCalculatorInitialize = function () {
 }
-var mc_riprap_size = function(y, a, v, g, z, s0, c, sgrock) {
-	var
-	d50,
-	hvmax = v * v * 1.33 * 1.33 / (2 * g) ;
-	if (s0 < 0.02) {
-		// Isbash
-		d50 = hvmax / (c * c * Math.cos(Math.atan(1 / z)) * (sgrock - 1));
-	} else if (s0 < 0.1) {
-		// Robinson unit q = v * y corrected 2015-10-17
-		d50 = 1.413 * Math.pow(v * y, 0.529) * Math.pow(s0, 0.794);
-	} else if (s0 < 0.4) {
-		// Robinson
-		d50 = 0.4623 * Math.pow(v * y, 0.529) * Math.pow(s0, 0.307);
-	} else {
-		d50 = '-';
-	}
-	return d50;
-};
 <?php echoCookieScript(); ?>
 </script>
 <?php
