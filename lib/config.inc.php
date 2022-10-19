17c2a975b23122c8d537f53fb0b9fd6b90f7c845
Tom Haws <tom.haws@gmail.com>
2022-10-19 01:15:16 -0700

    Remove https:// reference

--- lib/config.inc.php
+++ lib/config.inc.php
@@ -49,7 +49,7 @@ switch ($_SERVER['SERVER_NAME'])
 		define('DEBUG_MODE', true);
         break;    
     default :
-        $baseurl = 'https://www.hawsedc.com';
+        $baseurl = 'http://www.hawsedc.com';
 		define('DEBUG_MODE', false);
         break;
 }
