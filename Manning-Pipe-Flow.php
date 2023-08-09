edf7baabf50d6566d6496fd8b0a93da63a43b88e
Tom Haws <tom.haws@gmail.com>
2023-08-08 19:40:55 -0700

    More Spanish improvements.

--- Manning-Pipe-Flow.php
+++ Manning-Pipe-Flow.php
@@ -23,16 +23,20 @@ echoCalculatorForm(
 	),
 	//Results
 	Array(
-		Array('name' => 'q', 'units' => Array('m3ps','lps','mld','ft3ps','gpm','mgd'), 'label' => $ec_lang['mpf_flow']),
-		Array('name' => 'v', 'units' => Array('mps','ftps'), 'label' => $ec_lang['mpf_velocity']),
-		Array('name' => 'hv', 'units' => Array('mh2o','mmh2o','kpa','fth2o','inh2o','psi'), 'label' => $ec_lang['mpf_velocity_head']),
-		Array('name' => 'a', 'units' => Array('m2', 'mm2', 'ft2', 'in2'), 'label' => $ec_lang['mpf_flow_area']),
 		Array('name' => 'y', 'units' => Array('m','mm','ft','in'), 'label' => $ec_lang['mtc_flow_depth']),
+		Array('name' => 'a', 'units' => Array('m2', 'mm2', 'ft2', 'in2'), 'label' => $ec_lang['mpf_flow_area']),
+		Array('name' => 'a0', 'units' => Array('m2', 'mm2', 'ft2', 'in2'), 'label' => $ec_lang['mpf_pipe_area']),
+		Array('name' => 'aa0', 'units' => Array('depthFrac','depthPercent'), 'label' => $ec_lang['mpf_area_ratio']),
 		Array('name' => 'pw', 'units' => Array('m','mm','ft','in'), 'label' => $ec_lang['mpf_wetted_perimeter']),
 		Array('name' => 'rh', 'units' => Array('m','mm','ft','in'), 'label' => $ec_lang['mpf_hydraulic_radius']),
 		Array('name' => 't', 'units' => Array('m','mm','ft','in'), 'label' => $ec_lang['mpf_top_width']),
+		Array('name' => 'v', 'units' => Array('mps','ftps'), 'label' => $ec_lang['mpf_velocity']),
+		Array('name' => 'hv', 'units' => Array('mh2o','mmh2o','kpa','fth2o','inh2o','psi'), 'label' => $ec_lang['mpf_velocity_head']),
 		Array('name' => 'f', 'units' => NULL, 'label' => '<a href="https://www.engineeringtoolbox.com/froude-number-d_578.html">'.$ec_lang['mpf_froude_number'].'</a>'),
 		Array('name' => 'tau', 'units' => Array('npm2','psf'), 'label' => $ec_lang['mpf_shear_stress']),
+		Array('name' => 'q', 'units' => Array('m3ps','lps','mld','ft3ps','gpm','mgd'), 'label' => $ec_lang['mpf_flow']),
+		Array('name' => 'q0', 'units' => Array('m3ps','lps','mld','ft3ps','gpm','mgd'), 'label' => $ec_lang['mpf_full_flow']),
+		Array('name' => 'qq0', 'units' => Array('depthFrac','depthPercent'), 'label' => $ec_lang['mpf_full_flow_ratio']),
 	)
 );
 ?>
@@ -50,7 +54,7 @@ EngCalcs.pageCalculator = function(objForm) {
 	this.var = {};
 	this.var.c = 1.0;
 	this.var.g = 9.806;
-	// Read and convert form inputs to this.var.___ as SI units
+	// Read and convert form inputs to "this.var.___" as SI units
 	this.readFormInput(objForm, 'd0', hasUnits = true);
 	this.readFormInput(objForm, 's0', hasUnits = true);
 	this.readFormInput(objForm, 'n', hasUnits = false);
@@ -59,6 +63,8 @@ EngCalcs.pageCalculator = function(objForm) {
 	this.var.y = this.var.dd0 * this.var.d0;
 	this.var.theta = Math.acos(1 - 2 * this.var.dd0);
 	this.var.a = (this.var.theta - Math.sin(2 * this.var.theta) / 2) * Math.pow(this.var.d0, 2) / 4;
+	this.var.a0 = Math.PI * Math.pow(this.var.d0, 2) / 4;
+	this.var.aa0 = this.var.a / this.var.a0;
 	this.var.pw = this.var.theta * this.var.d0;
 	this.var.rh = this.var.d0 / (4 * this.var.theta) * (this.var.theta - Math.sin(this.var.theta) * Math.cos(this.var.theta));
 	this.var.t = this.var.d0 * Math.sin(this.var.theta);
@@ -67,20 +73,26 @@ EngCalcs.pageCalculator = function(objForm) {
 	this.var.q = this.var.v * this.var.a;
 	this.var.f = this.var.v * Math.sqrt(this.var.t/(this.var.g * this.var.a * Math.cos(Math.atan(this.var.s0))));
 	this.var.tau = this.var.rh * this.var.s0;
+	this.var.q0 = this.var.c / this.var.n * Math.PI * Math.pow(this.var.d0, 8/3) / Math.pow(4, 5/3) * Math.pow(this.var.s0, 0.5);
+	this.var.qq0 = this.var.q / this.var.q0;
 
-	this.writeFormResult(objForm, 'q', precision = 4, hasUnits = true);
-	this.writeFormResult(objForm, 'v', precision = 4, hasUnits = true);
-	this.writeFormResult(objForm, 'hv', precision = 4, hasUnits = true);
 	this.writeFormResult(objForm, 'y', precision = 4, hasUnits = true);
 	this.writeFormResult(objForm, 'a', precision = 4, hasUnits = true);
+	this.writeFormResult(objForm, 'a0', precision = 4, hasUnits = true);
+	this.writeFormResult(objForm, 'aa0', precision = 4, hasUnits = true);
 	this.writeFormResult(objForm, 'pw', precision = 4, hasUnits = true);
 	this.writeFormResult(objForm, 'rh', precision = 4, hasUnits = true);
 	this.writeFormResult(objForm, 't', precision = 4, hasUnits = true);
+	this.writeFormResult(objForm, 'v', precision = 4, hasUnits = true);
+	this.writeFormResult(objForm, 'hv', precision = 4, hasUnits = true);
 	this.writeFormResult(objForm, 'f', precision = 2, hasUnits = false);
 	this.writeFormResult(objForm, 'tau', precision = 4, hasUnits = true);
+	this.writeFormResult(objForm, 'q', precision = 4, hasUnits = true);
+	this.writeFormResult(objForm, 'q0', precision = 4, hasUnits = true);
+	this.writeFormResult(objForm, 'qq0', precision = 4, hasUnits = true);
 
 	// Sketch
-	this.var.gcr = 50; // Pipe circle radius
+	this.var.gcr = 50; // Graphic pipe circle radius
 	this.var.gh = 3 * this.var.gcr; // SVG height
 	this.var.gw = 3 * this.var.gcr; // SVG width
 	this.var.gcx = 1.5 * this.var.gcr; // Pipe center x
